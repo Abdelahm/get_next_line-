efb0ad2a5cb5bf0c7f7521a7da85370b39dcefb5
Abdelrahman Ahmed <abdelahm@c3r3s2.42wolfsburg.de>
2026-02-04 16:12:51 +0100

    Init

---
+++ get_next_line.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdelahm <abdelahm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/27 17:26:38 by abdelahm          #+#    #+#             */
+/*   Updated: 2026/01/27 18:10:45 by abdelahm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+
+	read(1, , 1);
+	malloc();
+	free();
+
+}
+
+int	main(void)
+{
+	open();
+}
+
+    //     #include <unistd.h>
+
+	//    ssize_t read(int fd, void *buf, size_t count);
+	//   int open(const char *pathname, int flags);
+	//   int open(const char *pathname, int flags, mode_t mode);
---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdelahm <abdelahm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/27 17:26:34 by abdelahm          #+#    #+#             */
+/*   Updated: 2026/01/27 18:09:58 by abdelahm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+char *get_next_line(int fd);
+
+#endif
